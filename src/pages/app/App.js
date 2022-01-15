6e8e22af132885f5901d598ce6bf8600ccb01d9f
Gustavo <gustavokkg2@hotmail.com>
2022-01-15 12:44:45 -0300

    setup de testes TDD

--- src/pages/app/App.js
+++ src/pages/app/App.js
@@ -1,5 +1,11 @@
+import NarutoImg from '../../images/naruto.png';
+
 export function App() {
   return(
-    <h1>Hello</h1>
+    <div>
+      <p>texto</p>
+      <button>quote no jutsu</button>
+      <img src={NarutoImg} alt='Naruto com uma kunai na mão'/>
+    </div>
   )
 }
